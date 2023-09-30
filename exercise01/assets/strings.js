d8f130489c5a655e99dda27fc0d33b6800aff90f
NelsonKinyanjui <nelsonkinyanjui246@gmail.com>
2023-09-30 13:52:13 +0300

    add sum total of coffee and bagel in dollars

--- exercise01/assets/strings.js
+++ exercise01/assets/strings.js
@@ -3,3 +3,8 @@ const called = 'my name is';
 
 let fullName = called + ' ' +  myName;
 console.log(fullName);
+
+const coffee = 5;
+const bagel = 3;
+const totalCost = `'Total cost : $' + (${coffee} + ${bagel})`;
+console.log(totalCost);
