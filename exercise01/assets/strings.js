214134c302842c2befe8350b403c33d32346cea7
NelsonKinyanjui <nelsonkinyanjui246@gmail.com>
2023-09-30 13:55:21 +0300

    remove simple code so as to practice complex code on the repository

--- exercise01/assets/strings.js
+++ exercise01/assets/strings.js
@@ -1,10 +1,1 @@
-const myName = 'Nelson';
-const called = 'my name is';
-
-let fullName = called + ' ' +  myName;
-console.log(fullName);
-
-const coffee = 5;
-const bagel = 3;
-const totalCost = `'Total cost : $' + (${coffee} + ${bagel})`;
-console.log(totalCost);
+ 
